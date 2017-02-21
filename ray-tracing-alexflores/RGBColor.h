f7c587c434e5bec093ba3e4f87484fe0dcca81b6
alexfloresgranado <axel_f_g@hotmail.com>
2017-02-21 17:53:08 -0400

    class RGBColor.h

---
+++ ray-tracing-alexflores/RGBColor.h
@@ -0,0 +1,15 @@
+#ifndef RGBCOLOR_H
+#define RGBCOLOR_H
+using namespace std;
+class RGBColor{
+private:
+	float green;
+	float red;
+	float blue;
+
+public:
+	RGBColor();
+    ~RGBColor();
+};
+
+#endif
